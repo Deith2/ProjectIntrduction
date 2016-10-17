02cf69c2aba5fbbea4ce0321e48ca6474fb4f1c3
Dariusz Łukasik <Dariusz.Lukasik@dreamlab.pl>
2016-10-17 22:24:46 +0200

    zmiana na src

--- Hobby/Hobby.Web/App_Start/NinjectWebCommon.cs
+++
@@ -1,2 +0,0 @@
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Hobby.Ninject.Commons.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Hobby.Ninject.Commons.NinjectWebCommon), "Stop")]
